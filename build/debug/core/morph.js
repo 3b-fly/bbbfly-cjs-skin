59baa56ed2b087284216a79320f498b873c869eb
3b-fly <member@3b-fly.eu>
2019-07-03 07:02:06 +0200

    Added morph skin themes image anchors support.
    - changed image "Src" to object
    - changed image definition to object
    - renamed image source definitions property to "Sources"
    - renamed image definitions property to "Images"

--- build/debug/core/morph.js
+++ build/debug/core/morph.js
@@ -73,40 +73,73 @@ bbbfly.morph.core._onInit = function(){
     var url = String.isString(theme.Lib)
       ? ngLibPath(theme.Lib,path) : path;
 
-    bbbfly.morph.core._recalcImagePaths(theme.Images,url);
-    bbbfly.morph.core._recalcImageSources(theme.ImageDefs,theme.Images);
+    bbbfly.morph.core._recalcImagePaths(theme.Sources,url);
+    bbbfly.morph.core._recalcImageSources(theme.Images,theme.Sources);
   }
 };
-bbbfly.morph.core._recalcImagePaths = function(images,url){
-  if(!Array.isArray(images)){return;}
+bbbfly.morph.core._recalcImagePaths = function(sources,url){
+  if(!Object.isObject(sources)){return;}
   if(!String.isString(url)){return;}
 
-  for(var i in images){
-    var imgPath = images[i];
+  for(var i in sources){
 
-    if(String.isString(imgPath)){
-      images[i] = url+imgPath;
+    var source = sources[i];
+    if(!Object.isObject(source)){return;}
+
+    if(String.isString(source.Path)){
+      source.Path = url+source.Path;
     }
   }
 };
-bbbfly.morph.core._recalcImageSources = function(def,images){
-  if(!Object.isObject(def)){return;}
-  if(!Array.isArray(images)){return;}
+bbbfly.morph.core._recalcImageSources = function(image,sources){
+  if(!Object.isObject(image)){return;}
+  if(!Object.isObject(sources)){return;}
 
-  for(var i in def){
-    var prop = def[i];
+  for(var i in image){
+    var prop = image[i];
 
     if((i === 'Src')){
-      if(Number.isInteger(prop)){
-        var path = images[prop];
-        if(!String.isString(path)){path = '';}
+      if(!Object.isObject(prop)){continue;}
 
-        ng_PreloadImage(path);
-        def[i] = path;
+      var source = sources[prop.Img];
+      if(!Object.isObject(source)){continue;}
+
+      var anchor = prop.Anchor;
+      if(String.isString(anchor) && Object.isObject(source.Anchors)){
+
+        bbbfly.morph.core._recalcImageAnchor(
+          image,source.Anchors[anchor]
+        );
       }
+
+      var path = source.Path;
+      if(!String.isString(path)){path = '';}
+      ng_PreloadImage(path);
+      image.Src = path;
     }
     else if(Object.isObject(prop)){
-      bbbfly.morph.core._recalcImageSources(prop,images);
+      bbbfly.morph.core._recalcImageSources(prop,sources);
+    }
+  }
+};
+bbbfly.morph.core._recalcImageAnchor = function(image,anchor){
+  if(!Object.isObject(image)){return;}
+  if(!Object.isObject(anchor)){return;}
+
+  var left = anchor.L;
+  var top = anchor.T;
+
+  if(!Number.isInteger(left)){left = 0;}
+  if(!Number.isInteger(top)){top = 0;}
+  if((left === 0) && (top === 0)){return;}
+
+  for(var i in image){
+    if(!Number.isInteger(image[i])){continue;}
+    if(!bbbfly.Morph.LTPattern.test(i)){continue;}
+
+    switch(i.slice(-1)){
+      case 'L': image[i] += left; break;
+      case 'T': image[i] += top; break;
     }
   }
 };
@@ -132,6 +165,7 @@ bbbfly.Morph = {
   _DefaultTheme: null,
   _ThemesCnt: 0,
   _Themes: {},
+  LTPattern: new RegExp('^[o]?[h]?[D]?[R]?[I]?[S|G]?[L|T]$'),
   SetDefaultTheme: bbbfly.morph.core._setDefaultTheme,
   RegisterTheme: bbbfly.morph.core._registerTheme,
   GetTheme: bbbfly.morph.core._getTheme,
