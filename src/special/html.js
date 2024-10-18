b34bc19c0060832171444ed121b61f216da418c0
3b-fly <3b-fly@users.noreply.github.com>
2024-10-18 23:20:04 +0200

    Added "bbbfly.morph.HTMLContentPanel" control (missing file).

---
+++ src/special/html.js
@@ -0,0 +1,39 @@
+﻿/**
+ * @file
+ * @author Jan Nejedly [support@3b-fly.eu]
+ * @copyright Jan Nejedly
+ *
+ * @inpackage html
+ */
+
+/** @ignore */
+var bbbfly = bbbfly || {};
+/** @ignore */
+bbbfly.morph = bbbfly.morph || {};
+
+/**
+ * @class
+ * @type control
+ * @extends bbbfly.HTMLContentPanel
+ * @implements bbbfly.Morph.Control
+ *
+ * @inpackage file
+ *
+ * @param {bbbfly.Panel.Definition} [def=undefined] - Descendant definition
+ * @param {object} [ref=undefined] - Reference owner
+ * @param {object|string} [parent=undefined] - Parent DIV element or it's ID
+ */
+bbbfly.morph.HTMLContentPanel = function(def,ref,parent){
+  bbbfly.morph.misc.ApplyControlClassName(def,'HTMLContentPanel');
+  return ngCreateControlAsType(def,'bbbfly.Panel',ref,parent);
+};
+
+/** @ignore */
+ngUserControls = ngUserControls || new Array();
+ngUserControls['bbbfly_morph_html'] = {
+  OnInit: function(){
+    bbbfly.Morph.RegisterControlType(
+      'bbbfly.morph.HTMLContentPanel',bbbfly.morph.HTMLContentPanel
+    );
+  }
+};
